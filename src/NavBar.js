2b259252c29a754db4d78d768fa1c5918af1281f
Stevie Ambroise <s.ambroise@protonmail.com>
2022-02-08 21:23:01 -0700

    Create navbar

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <nav className='nav-bar'>
+      <h2>Place For My Haikus</h2>
+      <button>My Favorite Poems</button>
+    </nav>
+  )
+}
+
+export default NavBar;
