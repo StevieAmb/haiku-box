876f07b0f9824c2e4499467dfbb91f17fe0be495
Stevie Ambroise <s.ambroise@protonmail.com>
2022-02-08 21:23:33 -0700

    Create form for inputting poem lines

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Form.css';
 
 class Form extends Component {
   constructor() {
@@ -12,7 +13,39 @@ class Form extends Component {
 
   render() {
     return (
-      <h2>This is working!</h2>
+      <form className='form'>
+        <label>
+          Line 1: 
+          <input
+            type='text'
+            placeholder='First line'
+            name='title'
+            value={this.state.lineOne}
+          />
+        </label>
+
+        <label>
+          Line 2: 
+          <input
+            type='text'
+            placeholder='Second line'
+            name='description'
+            value={this.state.lineTwo}
+          />
+        </label>
+
+        <label>
+          Line 3: 
+        <input
+          type='text'
+          placeholder='Last line'
+          name='description'
+          value={this.state.lineThree}
+        />
+        </label>
+
+        <button className='submit'>Write a Haiku!</button>
+      </form>
     )
   }
 }
